26b40498877f5c98b734bfa95b246286492559f3
ernestodelgadillopinilla <ernestoedelgadillo@hotmail.com>
2022-05-12 02:01:42 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace SumaAlgebraica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDato1 = new System.Windows.Forms.TextBox();
+            this.txtDato2 = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnMultiplica = new System.Windows.Forms.Button();
+            this.btnLimpia = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(103, 30);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(187, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "OPERACIONES MATEMÁTICAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dato 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dato 2";
+            // 
+            // txtDato1
+            // 
+            this.txtDato1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtDato1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDato1.Location = new System.Drawing.Point(157, 95);
+            this.txtDato1.Name = "txtDato1";
+            this.txtDato1.Size = new System.Drawing.Size(100, 20);
+            this.txtDato1.TabIndex = 3;
+            this.txtDato1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDato2
+            // 
+            this.txtDato2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtDato2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDato2.Location = new System.Drawing.Point(157, 138);
+            this.txtDato2.Name = "txtDato2";
+            this.txtDato2.Size = new System.Drawing.Size(100, 20);
+            this.txtDato2.TabIndex = 4;
+            this.txtDato2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.BackColor = System.Drawing.Color.Yellow;
+            this.btnSuma.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.btnSuma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSuma.Location = new System.Drawing.Point(12, 185);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(75, 23);
+            this.btnSuma.TabIndex = 5;
+            this.btnSuma.Text = "SUMAR";
+            this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txtResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.Color.Maroon;
+            this.txtResultado.Location = new System.Drawing.Point(157, 241);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnMultiplica
+            // 
+            this.btnMultiplica.BackColor = System.Drawing.Color.Yellow;
+            this.btnMultiplica.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplica.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplica.Location = new System.Drawing.Point(196, 188);
+            this.btnMultiplica.Name = "btnMultiplica";
+            this.btnMultiplica.Size = new System.Drawing.Size(90, 23);
+            this.btnMultiplica.TabIndex = 9;
+            this.btnMultiplica.Text = "MULTIPLICAR";
+            this.btnMultiplica.UseVisualStyleBackColor = false;
+            this.btnMultiplica.Click += new System.EventHandler(this.btnMultiplica_Click);
+            // 
+            // btnLimpia
+            // 
+            this.btnLimpia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLimpia.Location = new System.Drawing.Point(170, 291);
+            this.btnLimpia.Name = "btnLimpia";
+            this.btnLimpia.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpia.TabIndex = 10;
+            this.btnLimpia.Text = "LIMPIAR";
+            this.btnLimpia.UseVisualStyleBackColor = false;
+            this.btnLimpia.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.BackColor = System.Drawing.Color.Yellow;
+            this.btnResta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResta.Location = new System.Drawing.Point(106, 187);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(75, 23);
+            this.btnResta.TabIndex = 11;
+            this.btnResta.Text = "RESTAR";
+            this.btnResta.UseVisualStyleBackColor = false;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.Color.Yellow;
+            this.btnDivide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDivide.Location = new System.Drawing.Point(297, 188);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(74, 23);
+            this.btnDivide.TabIndex = 12;
+            this.btnDivide.Text = "DIVIDIR";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(383, 349);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnLimpia);
+            this.Controls.Add(this.btnMultiplica);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtDato2);
+            this.Controls.Add(this.txtDato1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.Text = "Operación matemática";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDato1;
+        private System.Windows.Forms.TextBox txtDato2;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnMultiplica;
+        private System.Windows.Forms.Button btnLimpia;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnDivide;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumaAlgebraica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            /*Para hacer la transformación de una variable tipo String a int,
+             * se utiliza la fórmula .Parse
+             */
+            int dt1, dt2, suma;
+            dt1 = int.Parse(txtDato1.Text);
+            dt2 = int.Parse(txtDato2.Text);
+            suma = dt1 + dt2;
+            /*Para cambiar los datos tipo int a String, se utiliza la fórmula
+             * .ToString
+             */
+            txtResultado.Text = suma.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtDato1.Text = "";
+            txtDato2.Text = "";
+            txtResultado.Text = "";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int dt1, dt2, resta;
+            dt1 = int.Parse(txtDato1.Text);
+            dt2 = int.Parse(txtDato2.Text);
+            resta = dt1 - dt2;
+            txtResultado.Text = resta.ToString();
+        }
+
+        private void btnMultiplica_Click(object sender, EventArgs e)
+        {
+            int dt1, dt2, multiplica;
+            dt1 = int.Parse(txtDato1.Text);
+            dt2 = int.Parse(txtDato2.Text);
+            multiplica = dt1 * dt2;
+            txtResultado.Text = multiplica.ToString();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            int dt1, dt2, resta;
+            dt1 = int.Parse(txtDato1.Text);
+            dt2 = int.Parse(txtDato2.Text);
+            resta = dt1 - dt2;
+            txtResultado.Text = resta.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            int dt1, dt2, divide;
+            dt1 = int.Parse(txtDato1.Text);
+            dt2 = int.Parse(txtDato2.Text);
+            divide = dt1 / dt2;
+            txtResultado.Text = divide.ToString();
+        }
+    }
+}
